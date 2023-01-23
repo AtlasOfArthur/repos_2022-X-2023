fdb4be371f1d4e4269af8c8d7fd23f7a82c4d0bd
arthur.harjama@gmail.com <arthur.harjama@gmail.com>
2023-01-23 17:04:43 +0200

    Muunto WindowsForm arabic to roman

---
+++ RomanNumberConverterForm/RomanNumberConverterForm/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace RomanNumberConverterForm
+{
+    partial class RomanConverter
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RomanConverter));
+            this.RoomalaisetNumerotLB = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.VastausLB = new System.Windows.Forms.Label();
+            this.MuunnaBT = new System.Windows.Forms.Button();
+            this.NumeroTB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // RoomalaisetNumerotLB
+            // 
+            this.RoomalaisetNumerotLB.AutoSize = true;
+            this.RoomalaisetNumerotLB.BackColor = System.Drawing.Color.Transparent;
+            this.RoomalaisetNumerotLB.Font = new System.Drawing.Font("Old English Text MT", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.RoomalaisetNumerotLB.ForeColor = System.Drawing.Color.Khaki;
+            this.RoomalaisetNumerotLB.Location = new System.Drawing.Point(43, 45);
+            this.RoomalaisetNumerotLB.Name = "RoomalaisetNumerotLB";
+            this.RoomalaisetNumerotLB.Size = new System.Drawing.Size(558, 71);
+            this.RoomalaisetNumerotLB.TabIndex = 0;
+            this.RoomalaisetNumerotLB.Text = "Roomalaiset numerot";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Old English Text MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(43, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(272, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anna numero väliltä 1 - 3999 \r\n";
+            // 
+            // VastausLB
+            // 
+            this.VastausLB.AutoSize = true;
+            this.VastausLB.BackColor = System.Drawing.Color.Transparent;
+            this.VastausLB.Font = new System.Drawing.Font("Bernard MT Condensed", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.VastausLB.ForeColor = System.Drawing.Color.Gold;
+            this.VastausLB.Location = new System.Drawing.Point(43, 264);
+            this.VastausLB.Name = "VastausLB";
+            this.VastausLB.Size = new System.Drawing.Size(85, 28);
+            this.VastausLB.TabIndex = 2;
+            this.VastausLB.Text = "Vastaus";
+            this.VastausLB.Visible = false;
+            // 
+            // MuunnaBT
+            // 
+            this.MuunnaBT.BackColor = System.Drawing.Color.Black;
+            this.MuunnaBT.Font = new System.Drawing.Font("Bernard MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.MuunnaBT.ForeColor = System.Drawing.Color.Khaki;
+            this.MuunnaBT.Location = new System.Drawing.Point(205, 176);
+            this.MuunnaBT.Name = "MuunnaBT";
+            this.MuunnaBT.Size = new System.Drawing.Size(110, 41);
+            this.MuunnaBT.TabIndex = 3;
+            this.MuunnaBT.Text = "Muunna";
+            this.MuunnaBT.UseVisualStyleBackColor = false;
+            this.MuunnaBT.Click += new System.EventHandler(this.MuunnaBT_Click);
+            // 
+            // NumeroTB
+            // 
+            this.NumeroTB.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumeroTB.Location = new System.Drawing.Point(43, 178);
+            this.NumeroTB.Name = "NumeroTB";
+            this.NumeroTB.Size = new System.Drawing.Size(125, 34);
+            this.NumeroTB.TabIndex = 4;
+            // 
+            // RomanConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(639, 363);
+            this.Controls.Add(this.NumeroTB);
+            this.Controls.Add(this.MuunnaBT);
+            this.Controls.Add(this.VastausLB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.RoomalaisetNumerotLB);
+            this.Name = "RomanConverter";
+            this.Text = "Numeron muutos arabialaisesta roomalaiseksi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label RoomalaisetNumerotLB;
+        private Label label2;
+        private Label VastausLB;
+        private Button MuunnaBT;
+        private TextBox NumeroTB;
+    }
+}
---
+++ RomanNumberConverterForm/RomanNumberConverterForm/Form1.cs
@@ -0,0 +1,175 @@
+namespace RomanNumberConverterForm
+{
+    public partial class RomanConverter : Form
+    {
+        public RomanConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void MuunnaBT_Click(object sender, EventArgs e)
+        {
+            int tuhannet, sadat, kympit, yks;
+            string vastaus = "";
+            if (NumeroTB.Text.Length > 3 )// Pidempi kuin 3, kyseessä on tuhat luku
+            {
+                tuhannet = Convert.ToInt32(NumeroTB.Text.Substring(0, 1));
+                sadat = Convert.ToInt32(NumeroTB.Text.Substring(1, 1));
+                kympit = Convert.ToInt32(NumeroTB.Text.Substring(2, 1));
+                yks = Convert.ToInt32(NumeroTB.Text.Substring(3, 1));
+
+                if (tuhannet % 3 == 0)
+                {
+                    vastaus += "MMM";
+                }
+                else if (tuhannet % 2 == 0)
+                {
+                    vastaus += "MM";
+                }
+                else if (tuhannet % 1 == 0) 
+                {
+                    vastaus += "M";
+                }
+                else
+                {
+                    vastaus += "";
+                }
+                vastaus += Sataset(sadat, vastaus);
+                vastaus += Kymmenet(kympit, vastaus);
+                vastaus += Ykkoset(yks, vastaus);
+                VastausLB.Text = vastaus;
+                VastausLB.Visible = true;
+            }
+        }
+        private string Sataset(int sadat, string vastaus)
+        {
+            if (sadat % 9 == 0 && sadat != 0)
+            {
+                return "CM"; // C= 100, koska M kirjaimen(M=1000) oikealla puolella CM=900
+            }
+            else if (sadat % 8 == 0 && sadat != 0)
+            {
+                return "DCCC"; // D=500 C=100
+            }
+            else if (sadat % 7 == 0 && sadat != 0)
+            {
+                return "DCC"; 
+            }
+            else if (sadat % 6 == 0 && sadat != 0)
+            {
+                return "DC";
+            }
+            else if (sadat % 5 == 0 && sadat != 0)
+            {
+                return "D";
+            }
+            else if (sadat % 4 == 0 && sadat != 0)
+            {
+                return "CD";
+            }
+            else if (sadat % 3 == 0 && sadat != 0)
+            {
+                return "CCC";
+            }
+            else if (sadat % 2 == 0 && sadat != 0)
+            {
+                return "CC";
+            }
+            else if (sadat % 1 == 0 && sadat != 0)
+            {
+                return "C";
+            }
+            else
+            {
+                return "";
+            }
+        }
+        private string Kymmenet(int kympit, string vastaus)
+        {
+            if (kympit % 9 == 0 && kympit != 0)
+            {
+                return "XC"; // X=10 C=100 
+            }
+            else if (kympit % 8 == 0 && kympit != 0)
+            {
+                return "LXXX"; // L=50
+            }
+            else if (kympit % 7 == 0 && kympit != 0)
+            {
+                return "LXX";
+            }
+            else if (kympit % 6 == 0 && kympit != 0)
+            {
+                return "LX"; 
+            }
+            else if (kympit % 5 == 0 && kympit != 0)
+            {
+                return "L";
+            }
+            else if (kympit % 4 == 0 && kympit != 0)
+            {
+                return "XL";
+            }
+            else if (kympit % 3 == 0 && kympit != 0)
+            {
+                return "XXX";
+            }
+            else if (kympit % 2 == 0 && kympit != 0)
+            {
+                return "XX";
+            }
+            else if (kympit % 1 == 0 && kympit != 0)
+            {
+                return "X";
+            }
+            else
+            {
+                return "";
+            }
+        }
+        private string Ykkoset(int yks, string vastaus)
+        {
+            if (yks % 9 == 0 && yks != 0)
+            {
+                return "IX"; // I=1 X=10
+            }
+            else if (yks % 8 == 0 && yks != 0)
+            {
+                return "VIII"; // V=5 5+3=8
+            }
+            else if (yks % 7 == 0 && yks != 0)
+            {
+                return "VII";
+            }
+            else if (yks % 6 == 0 && yks != 0)
+            {
+                return "VI";
+            }
+            else if (yks % 5 == 0 && yks != 0)
+            {
+                return "V";
+            }
+            else if (yks % 4 == 0 && yks != 0)
+            {
+                return "IV"; // 5-1=4
+            }
+            else if (yks % 3 == 0 && yks != 0)
+            {
+                return "III";
+            }
+            else if (yks % 2 == 0 && yks != 0)
+            {
+                return "II";
+            }
+            else if (yks % 1 == 0 && yks != 0)
+            {
+                return "I";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+    
+}
