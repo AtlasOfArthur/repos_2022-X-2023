e535b8af389aded86d5999671ea19e87508e66e1
arthur.harjama@gmail.com <arthur.harjama@gmail.com>
2023-01-24 16:04:14 +0200

    try{} catch{} Bonuksineen, toimii

--- C_To_F_To_C-MuunninForm/C_To_F_To_C-MuunninForm/Form1.cs
+++ C_To_F_To_C-MuunninForm/C_To_F_To_C-MuunninForm/Form1.cs
@@ -9,24 +9,31 @@ namespace C_To_F_To_C_MuunninForm
 
         private void muunnaBT_Click(object sender, EventArgs e)
         {
-            double vastaus;
-            double asteet = Convert.ToDouble(asteetTB.Text); // Tämä rivi oli virheellinen
+            try // Yritetään uudelleen: try, catch ilman hölmöjä virheitä, jos vaikka toimisi nyt
+            {
+                double vastaus;
+                double asteet = Convert.ToDouble(asteetTB.Text); // Tämä rivi oli virheellinen
 
-            if (celsiusRButton.Checked)
-            {
-                vastaus = asteet * 1.8 + 32;
-                vastausLB.Text = asteet + " Fahrenheitia on " + vastaus + " Celsius astetta.";
+                if (celsiusRButton.Checked)
+                {
+                    vastaus = asteet * 1.8 + 32;
+                    vastausLB.Text = asteet + " Fahrenheitia on " + vastaus + " Celsius astetta.";
+                }
+                else if (fahrenheitRButton.Checked)
+                {
+                    vastaus = asteet - 32 / 1.8;
+                    vastausLB.Text = asteet + " Celsiusta on " + vastaus + " Fahrenheit astetta";
+                }
+                else
+                {
+                    vastausLB.Text = "Lukua ei pysty muuntamaan. Anna toinen."; // Puolipiste puuttui
+                }
+                vastausLB.Visible= true; // Ohjeessa oli että tämä laitettiin kaikkien iffauksien loppuun. Toimisi kai näinkin.
             }
-            else if (fahrenheitRButton.Checked)
+            catch (Exception ex)
             {
-                vastaus = asteet - 32 / 1.8;
-                vastausLB.Text = asteet + " Celsiusta on " + vastaus + " Fahrenheit astetta";
+                MessageBox.Show(ex.Message + " Anna kelvollinen luku.");
             }
-            else
-            {
-                vastausLB.Text = "Lukua ei pysty muuntamaan. Anna toinen."; // Puolipiste puuttui
-            }
-            vastausLB.Visible= true; // Ohjeessa oli että tämä laitettiin kaikkien iffauksien loppuun. Toimisi kai näinkin.
         }
     }
 }
