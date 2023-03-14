ab5798621fa0b52f1e9b284d6e38f818123c8ff6
AtlasOfArthur <arthur.harjama@gmail.com>
2023-03-14 13:28:12 +0200

    Aliohjelma2 rautalangasta vääntämällä, kun ei opettajilta saa apua. (Ohjelma toimii nyt, mutta pitää muunta vielä se aliohjelmaksi

--- 13 Aliohjelma2/13 Aliohjelma2/Program.cs
+++ 13 Aliohjelma2/13 Aliohjelma2/Program.cs
@@ -1,32 +1,59 @@
-﻿
+﻿int kayttäjanluku;
+int lisaa1, lisaa2, lisaa3;
+int summa = 0;
 
+List<int> list = new List<int>();
+
+for (int i = 1; i < 4; i++)
+{
+    Console.WriteLine("Anna " + i + " luku.");
+    list.Add(int.Parse(Console.ReadLine()));
+    summa = list.Sum();
+}
+
+Console.WriteLine("Antamiesi lukujen " + list[0] + ", " + list[1] + ", " + list[2] + " summa on " + summa);
+
+// Nyt mutetaan aliohjelmaksi ... Push to git
+
+
+    
+
+
+/*
 static int KolmenLuvunSumma(int nro1, int nro2, int nro3, int summa) //Aliohjelma (Eng = Method / Function)
 {
-
-    Console.WriteLine("Anna kolme lukua niin lasken niiden summan.\n");
+    
 
     for (int i = 0; i < 3; i++)
     {
-        if (i == 0)
-        {
-            Console.WriteLine("Anna " + i +" luku \n \n");
-            nro1 = int.Parse(Console.ReadLine());    // Tallentaa käyttäjän antaman numeron
-            summa+= nro1;
-        }
-        else if (i == 1)
-        {
-            Console.WriteLine("Anna " + i + " luku \n \n");
-            nro2 = int.Parse(Console.ReadLine());
-            summa+= nro2;
-        }
-        else if (i == 2)
-        {
-            Console.WriteLine("Anna " + i + " luku \n \n");
-            nro3 = int.Parse(Console.ReadLine());
-            summa+= nro3;
-        }
+
     }
-    return summa;
+
 }
 
+Console.WriteLine("Anna kolme lukua niin lasken niiden summan.\n");
 Console.WriteLine(KolmenLuvunSumma());
+*/
+/*
+for (int i = 0; i < 3; i++)
+{
+    if (i == 0)
+    {
+        Console.WriteLine("Anna " + i + " luku \n \n");
+        nro1 = int.Parse(Console.ReadLine());    // Tallentaa käyttäjän antaman numeron
+        summa += nro1;
+    }
+    else if (i == 1)
+    {
+        Console.WriteLine("Anna " + i + " luku \n \n");
+        nro2 = int.Parse(Console.ReadLine());
+        summa += nro2;
+    }
+    else if (i == 2)
+    {
+        Console.WriteLine("Anna " + i + " luku \n \n");
+        nro3 = int.Parse(Console.ReadLine());
+        summa += nro3;
+    }
+}
+return summa; */
