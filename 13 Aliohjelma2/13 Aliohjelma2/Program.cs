43c0409c2a57ec12158d4c1512138f500cf6a041
AtlasOfArthur <arthur.harjama@gmail.com>
2023-03-14 15:27:46 +0200

    Toimii vihdoin. Kiitos jyväskylän amk. ja Tarja !

--- 13 Aliohjelma2/13 Aliohjelma2/Program.cs
+++ 13 Aliohjelma2/13 Aliohjelma2/Program.cs
@@ -1,6 +1,15 @@
-﻿int kayttäjanluku;
-int lisaa1, lisaa2, lisaa3;
-int summa = 0;
+﻿
+
+
+Console.WriteLine("Tehtävä 13. Laske aliohjelmalla kolmen luvun summa. \n");
+
+
+
+static int KolmenLuvunSumma (int luku1, int luku2, int luku3)
+{
+    return luku1 + luku2 + luku3;
+}
+
 
 List<int> list = new List<int>();
 
@@ -8,18 +17,35 @@ for (int i = 1; i < 4; i++)
 {
     Console.WriteLine("Anna " + i + " luku.");
     list.Add(int.Parse(Console.ReadLine()));
-    summa = list.Sum();
 }
 
-Console.WriteLine("Antamiesi lukujen " + list[0] + ", " + list[1] + ", " + list[2] + " summa on " + summa);
+Console.WriteLine(KolmenLuvunSumma(list[0], list[1], list[2]));
 
-// Nyt mutetaan aliohjelmaksi ... Push to git
 
+/*
+static int KolmenLuvunSumma (int list, int summa);
+{
+
+
+for (int i = 1; i < 4; i++)
+{
+    Console.WriteLine("Anna " + i + " luku.");
+    list.Add(int.Parse(Console.ReadLine()));
+    
+}
+    return ( list[0] +  list[1] + list[2] );
+}
+*/
+
+
+//    list.Sum();
+// Console.WriteLine(KolmenLuvunSumma());
+// Console.WriteLine("Antamiesi lukujen " + list[0] + ", " + list[1] + ", " + list[2] + " summa on " + summa);
 
     
 
 
-/*
+/* // (Vanhat)
 static int KolmenLuvunSumma(int nro1, int nro2, int nro3, int summa) //Aliohjelma (Eng = Method / Function)
 {
     
